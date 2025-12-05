31bd0e7632b945ffc098b73bb056c27843c7bf16
Rakhshan Ali <alirakhshan7070@gmail.com>
2025-12-05 14:27:14 +0000

    mumba: Fix failing symlinks - It's there in the init command but it fails to symlink at the init so let's do it the other way.
    01-02 15:21:06.160     1     1 I init    : Command 'symlink /vendor/firmware_mnt /firmware' action=early-init (/vendor/etc/init/hw/init.qcom.rc:36) took 1ms and failed: symlink() failed: Read-only file system
    01-02 15:21:06.160     1     1 I init    : Command 'symlink /vendor/bt_firmware /bt_firmware' action=early-init (/vendor/etc/init/hw/init.qcom.rc:37) took 0ms and failed: symlink() failed: Read-only file system
    01-02 15:21:06.161     1     1 I init    : Command 'symlink /vendor/dsp /dsp' action=early-init (/vendor/etc/init/hw/init.qcom.rc:38) took 0ms and failed: symlink() failed: Read-only file system
    
    Signed-off-by: romgharti <romgharti@gmail.com>
    Signed-off-by: n0escape-exe <viaan4542@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -168,6 +168,11 @@ TARGET_BOARD_PLATFORM := parrot
 BOOT_SECURITY_PATCH := 2026-07-01
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
+BOARD_ROOT_EXTRA_SYMLINKS := \
+    /vendor/firmware_mnt:/firmware \
+    /vendor/bt_firmware:/bt_firmware \
+    /vendor/dsp:/dsp
+
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/configs/props/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/props/product.prop
