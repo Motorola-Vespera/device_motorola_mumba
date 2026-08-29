3caf0926bc4c6351cfa0862bfd75df6d68691fe9
zaraknyu <razenxizu@gmail.com>
2026-08-29 20:23:43 +0000

    mubma: Add EuiccOverlayMumba for all varients of mumba
    Signed-off-by: zaraknyu <razenxizu@gmail.com>

--- device.mk
+++ device.mk
@@ -224,6 +224,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
+    EuiccOverlayMumba \
     SystemUIResMumba \
     WifiResMumba \
     FrameworksResMumba \
